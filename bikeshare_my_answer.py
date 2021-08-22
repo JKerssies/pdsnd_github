a9ff1d2b34a89f9aaed1290cb84e177e1a811934
JEROEN KERSSIES <jeroen.kerssies@foodpairing.com>
2021-08-22 13:02:51 +0200

    made messages look nice

--- bikeshare_my_answer.py
+++ bikeshare_my_answer.py
@@ -274,7 +274,7 @@ def main():
         user_stats(df)
         data(df)
  
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart the analysis? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
     
